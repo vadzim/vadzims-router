6869939eef30942e1076d1e652b3970d646ab6ae
Vadzim Zieńka <v@vadzim.info>
2020-09-22 15:14:30 +0300

    log listening

--- ipv6proxy.js
+++ ipv6proxy.js
@@ -2,5 +2,6 @@ import net from "net"
 
 net.createServer()
 	.on("error", e => console.error(e))
+	.on("listening", () => console.log("Proxy server is listening"))
 	.on("connection", socket => socket.pipe(net.connect(80, "127.0.0.1")).pipe(socket))
 	.listen({ ipv6Only: true, port: 80, host: "::" })
